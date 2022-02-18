d9d142cb669006a53c594f0a1ab4866a1cc75b06
Martin <martin.e.castro.88@gmail.com>
2022-02-18 19:09:19 -0300

    Bonus control

--- app/src/main/java/com/example/horsegame/MainActivity.kt
+++ app/src/main/java/com/example/horsegame/MainActivity.kt
@@ -13,10 +13,17 @@ import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
+    private var width_bonus = 0
+
     private var cellSelected_x = 0
     private var cellSelected_y = 0
 
+    private var levelMoves = 64
+    private var movesRequired = 4
+    private var moves = 64
     private var options = 0
+    private var bonus = 0
+
     private var nameColorBlack = "black_cell"
     private var nameColorWhite = "white_cell"
 
@@ -94,6 +101,8 @@ class MainActivity : AppCompatActivity() {
         val width_cell = (width_dp-lateralMarginDP)/8
         val height_cell = width_cell
 
+        width_bonus = 2 * width_cell.toInt()
+
         for(i in 0..7) {
             for (j in 0..7) {
                 iv = findViewById(resources.getIdentifier("c$i$j","id",packageName))
@@ -122,27 +131,49 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun selectCell(x:Int, y:Int){
+        moves--
+        var tvMovesData = findViewById<TextView>(R.id.tvMovesData)
+        tvMovesData.text = moves.toString()
+
+        growProgressBonus()
+
+        if(board[x][y] == 2) {
+            bonus++
+            var tvBonusData = findViewById<TextView>(R.id.tvBonusData)
+            tvBonusData.text = " + ${bonus}"
+        }
+
         board[x][y] = 1
         paintHorseCell(cellSelected_x,cellSelected_y,"previous_cell")
+
         cellSelected_x = x
         cellSelected_y = y
+
         clearOptions()
+
         paintHorseCell(x,y,"selected_cell")
         checkOption(x,y)
+
+        if(moves > 0) {
+            checkNewBonus()
+            //checkGameOver(x,y)
+        }
+        //else
+            //checkSuccessfulEnd()
     }
 
-    private fun clearOption(x:Int, y:Int){
-        var iv: ImageView = findViewById(resources.getIdentifier("c$x$y","id",packageName))
-        //black cell
-        if (checkColorCell(x,y) == "black")
-            iv.setBackgroundColor(ContextCompat.getColor(this,resources.getIdentifier(nameColorBlack,"color", packageName)))
-        //white cell
-        else
-            iv.setBackgroundColor(ContextCompat.getColor(this,resources.getIdentifier(nameColorWhite,"color", packageName)))
-        //horse
-        if(board[x][y] == 1)
-            iv.setBackgroundColor(ContextCompat.getColor(this,resources.getIdentifier("previous_cell","color", packageName)))
+    private fun growProgressBonus(){
+        var moves_done = levelMoves - moves
+        var bonus_done = moves_done / movesRequired
+        var moves_reset = movesRequired * bonus_done
+        var bonus_grow = moves_done - moves_reset
 
+        var v = findViewById<View>(R.id.vNewBonus)
+        var widthBonus = ((width_bonus/movesRequired) * bonus_grow).toFloat()
+
+        var height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8f, resources.displayMetrics).toInt()
+        var width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, widthBonus, resources.displayMetrics).toInt()
+        v.setLayoutParams(TableRow.LayoutParams(width,height))
     }
 
     private fun clearOptions(){
@@ -155,6 +186,12 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun paintHorseCell(x:Int, y:Int, color:String){
+        var iv: ImageView = findViewById(resources.getIdentifier("c$x$y","id",packageName))
+        iv.setBackgroundColor(ContextCompat.getColor(this,resources.getIdentifier(color,"color", packageName)))
+        iv.setImageResource(R.drawable.ic_horse)
     }
 
     private fun checkOption(x:Int, y:Int){
@@ -180,7 +217,8 @@ class MainActivity : AppCompatActivity() {
             if(board[option_x][option_y] == 0 || board[option_x][option_y] == 2){
                 options++
                 paintOptions(option_x,option_y)
-                board[option_x][option_y] = 9
+                if (board[option_x][option_y] == 0)
+                    board[option_x][option_y] = 9
             }
 
     }
@@ -197,13 +235,42 @@ class MainActivity : AppCompatActivity() {
         var blackRow_x = arrayOf(1,3,5,7)
         if ((blackColumn_x.contains(x) && blackColumn_x.contains(y))
             || (blackRow_x.contains(x) && blackRow_x.contains(y)))
-                color = "black"
+            color = "black"
         return color
     }
 
-    private fun paintHorseCell(x:Int, y:Int, color:String){
+    private fun clearOption(x:Int, y:Int){
         var iv: ImageView = findViewById(resources.getIdentifier("c$x$y","id",packageName))
-        iv.setBackgroundColor(ContextCompat.getColor(this,resources.getIdentifier(color,"color", packageName)))
-        iv.setImageResource(R.drawable.ic_horse)
+        //black cell
+        if (checkColorCell(x,y) == "black")
+            iv.setBackgroundColor(ContextCompat.getColor(this,resources.getIdentifier(nameColorBlack,"color", packageName)))
+        //white cell
+        else
+            iv.setBackgroundColor(ContextCompat.getColor(this,resources.getIdentifier(nameColorWhite,"color", packageName)))
+        //horse
+        if(board[x][y] == 1)
+            iv.setBackgroundColor(ContextCompat.getColor(this,resources.getIdentifier("previous_cell","color", packageName)))
+
+    }
+
+    private fun checkNewBonus(){
+        if(moves % movesRequired == 0) {
+            var bonusCell_x = 0
+            var bonusCell_y = 0
+
+            var bonusCell = false
+            while (!bonusCell){
+                bonusCell_x = (0..7).random()
+                bonusCell_y = (0..7).random()
+                if(board[bonusCell_x][bonusCell_y] == 0) bonusCell = true
+            }
+            board[bonusCell_x][bonusCell_y] = 2
+            paintBonusCell(bonusCell_x, bonusCell_y)
+        }
+    }
+
+    private fun paintBonusCell(x:Int, y:Int){
+        var iv: ImageView = findViewById(resources.getIdentifier("c$x$y","id",packageName))
+        iv.setImageResource(R.drawable.ic_bonus)
     }
 }
