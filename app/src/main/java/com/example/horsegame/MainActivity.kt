7d38b751d708297678d3cc3c02b1ead2a2673953
Martin <martin.e.castro.88@gmail.com>
2022-02-21 23:27:48 -0300

    moves control management

--- app/src/main/java/com/example/horsegame/MainActivity.kt
+++ app/src/main/java/com/example/horsegame/MainActivity.kt
@@ -3,14 +3,12 @@ package com.example.horsegame
 import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Layout
 import android.util.TypedValue
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TableRow
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +24,8 @@ class MainActivity : AppCompatActivity() {
     private var options = 0
     private var bonus = 0
 
+    private var checkMovement = true
+
     private var nameColorBlack = "black_cell"
     private var nameColorWhite = "white_cell"
 
@@ -40,54 +40,10 @@ class MainActivity : AppCompatActivity() {
         setFirstPosition()
     }
 
-    fun checkCellClicked(v: View) {
-        var name = v.tag.toString()
-        var x = name.subSequence(1,2).toString().toInt()
-        var y = name.subSequence(2,3).toString().toInt()
-        checkCell(x,y)
-    }
-
-    private fun checkCell(x:Int, y:Int){
-        var dif_x = x- cellSelected_x
-        var dif_y = y - cellSelected_y
-
-        var checkTrue = false
-        if (dif_x == 1 && dif_y == 2) checkTrue = true
-        if (dif_x == 1 && dif_y == -2) checkTrue = true
-        if (dif_x == 2 && dif_y == 1) checkTrue = true
-        if (dif_x == 2 && dif_y == -1) checkTrue = true
-        if (dif_x == -1 && dif_y == 2) checkTrue = true
-        if (dif_x == -1 && dif_y == -2) checkTrue = true
-        if (dif_x == -2 && dif_y == 1) checkTrue = true
-        if (dif_x == -2 && dif_y == -1) checkTrue = true
-
-        if(board[x][y] == 1) checkTrue = false
-
-        if(checkTrue) selectCell(x,y)
-    }
-
     private fun initScreenGame(){
         setSizeBoard()
         hideMessage()
     }
-
-    private  fun resetBoard(){
-        // 0 está libre
-        // 1 casilla marcada
-        // 2 bonus
-        // 9 opcion del movimieto actual
-        board = arrayOf(
-            intArrayOf(0,0,0,0,0,0,0,0),
-            intArrayOf(0,0,0,0,0,0,0,0),
-            intArrayOf(0,0,0,0,0,0,0,0),
-            intArrayOf(0,0,0,0,0,0,0,0),
-            intArrayOf(0,0,0,0,0,0,0,0),
-            intArrayOf(0,0,0,0,0,0,0,0),
-            intArrayOf(0,0,0,0,0,0,0,0),
-            intArrayOf(0,0,0,0,0,0,0,0)
-        )
-    }
-
     private fun setSizeBoard(){
         var iv: ImageView
         //save display
@@ -116,22 +72,47 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
     private fun hideMessage(){
         var lyMessage = findViewById<LinearLayout>(R.id.lyMessage)
         lyMessage.visibility = View.INVISIBLE
     }
 
-    private fun setFirstPosition(){
-        var x = 0
-        var y = 0
-        x = (0..7).random()
-        y = (0..7).random()
-        cellSelected_x = x
-        cellSelected_y = y
-        selectCell(x,y)
+    fun checkCellClicked(v: View) {
+        var name = v.tag.toString()
+        var x = name.subSequence(1,2).toString().toInt()
+        var y = name.subSequence(2,3).toString().toInt()
+        checkCell(x,y)
     }
+    private fun checkCell(x:Int, y:Int){
+        var checkTrue = true
+        if(checkMovement){
+            var dif_x = x- cellSelected_x
+            var dif_y = y - cellSelected_y
 
+            checkTrue = false
+            if (dif_x == 1 && dif_y == 2) checkTrue = true
+            if (dif_x == 1 && dif_y == -2) checkTrue = true
+            if (dif_x == 2 && dif_y == 1) checkTrue = true
+            if (dif_x == 2 && dif_y == -1) checkTrue = true
+            if (dif_x == -1 && dif_y == 2) checkTrue = true
+            if (dif_x == -1 && dif_y == -2) checkTrue = true
+            if (dif_x == -2 && dif_y == 1) checkTrue = true
+            if (dif_x == -2 && dif_y == -1) checkTrue = true
+        }
+        else{
+            if(board[x][y] != 1){
+                bonus--
+                var tvBonusData = findViewById<TextView>(R.id.tvBonusData)
+                tvBonusData.text = " + $bonus"
+                if(bonus == 0)
+                    tvBonusData.text = ""
+            }
+        }
+
+        if(board[x][y] == 1) checkTrue = false
+
+        if(checkTrue) selectCell(x,y)
+    }
     private fun selectCell(x:Int, y:Int){
         moves--
         var tvMovesData = findViewById<TextView>(R.id.tvMovesData)
@@ -154,105 +135,41 @@ class MainActivity : AppCompatActivity() {
         clearOptions()
 
         paintHorseCell(x,y,"selected_cell")
+        checkMovement = true
         checkOption(x,y)
 
         if(moves > 0) {
             checkNewBonus()
-            checkGameOver(x,y)
+            checkGameOver()
         }
         else
             showMessage ("You win", "Next level", false)
     }
 
-    private fun growProgressBonus(){
-        var moves_done = levelMoves - moves
-        var bonus_done = moves_done / movesRequired
-        var moves_reset = movesRequired * bonus_done
-        var bonus_grow = moves_done - moves_reset
-
-        var v = findViewById<View>(R.id.vNewBonus)
-        var widthBonus = ((width_bonus/movesRequired) * bonus_grow).toFloat()
-
-        var height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8f, resources.displayMetrics).toInt()
-        var width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, widthBonus, resources.displayMetrics).toInt()
-        v.setLayoutParams(TableRow.LayoutParams(width,height))
+    private  fun resetBoard(){
+        // 0 está libre
+        // 1 casilla marcada
+        // 2 bonus
+        // 9 opcion del movimieto actual
+        board = arrayOf(
+            intArrayOf(0,0,0,0,0,0,0,0),
+            intArrayOf(0,0,0,0,0,0,0,0),
+            intArrayOf(0,0,0,0,0,0,0,0),
+            intArrayOf(0,0,0,0,0,0,0,0),
+            intArrayOf(0,0,0,0,0,0,0,0),
+            intArrayOf(0,0,0,0,0,0,0,0),
+            intArrayOf(0,0,0,0,0,0,0,0),
+            intArrayOf(0,0,0,0,0,0,0,0)
+        )
     }
-
-    private fun clearOptions(){
-        for(i in 0..7){
-            for(j in 0..7){
-                if(board[i][j] == 9 || board[i][j] == 2){
-                    if(board[i][j] == 9)
-                        board[i][j] = 0
-                    clearOption(i,j)
-                }
-            }
-        }
-    }
-
-    private fun paintHorseCell(x:Int, y:Int, color:String){
-        var iv: ImageView = findViewById(resources.getIdentifier("c$x$y","id",packageName))
-        iv.setBackgroundColor(ContextCompat.getColor(this,resources.getIdentifier(color,"color", packageName)))
-        iv.setImageResource(R.drawable.ic_horse)
-    }
-
-    private fun checkOption(x:Int, y:Int){
-        options = 0
-        checkMove(x,y,1,2)      //1 right, 2 up
-        checkMove(x,y,2,1)      //2 right, 1 up
-        checkMove(x,y,1,-2)     //1 right, 2 bottom
-        checkMove(x,y,2,-1)     //2 right, 1 bottom
-        checkMove(x,y,-1,2)     //1 left, 2 up
-        checkMove(x,y,-2,1)     //2 left, 1 up
-        checkMove(x,y,-1,-2)    //1 left, 2 bottom
-        checkMove(x,y,-2,-1)    //2 left, 1 bottom
-
-        var tvOptionsData = findViewById<TextView>(R.id.tvOptionsData)
-        tvOptionsData.text = options.toString()
-    }
-
-    private fun checkMove(x:Int, y:Int, move_x:Int, move_y:Int){
-        var option_x = x + move_x
-        var option_y = y + move_y
-        //check if the move is not out of the board
-        if (option_x < 8 && option_y < 8 && option_x >= 0 && option_y >= 0)
-            if(board[option_x][option_y] == 0 || board[option_x][option_y] == 2){
-                options++
-                paintOptions(option_x,option_y)
-                if (board[option_x][option_y] == 0)
-                    board[option_x][option_y] = 9
-            }
-
-    }
-
-    private fun paintOptions(x:Int, y:Int){
-        var iv: ImageView = findViewById(resources.getIdentifier("c$x$y","id",packageName))
-        if(checkColorCell(x,y) == "black") iv.setBackgroundResource(R.drawable.option_black)
-        else iv.setBackgroundResource(R.drawable.option_white)
-    }
-
-    private fun checkColorCell(x:Int, y:Int):String{
-        var color = "white"
-        var blackColumn_x = arrayOf(0,2,4,6)
-        var blackRow_x = arrayOf(1,3,5,7)
-        if ((blackColumn_x.contains(x) && blackColumn_x.contains(y))
-            || (blackRow_x.contains(x) && blackRow_x.contains(y)))
-            color = "black"
-        return color
-    }
-
-    private fun clearOption(x:Int, y:Int){
-        var iv: ImageView = findViewById(resources.getIdentifier("c$x$y","id",packageName))
-        //black cell
-        if (checkColorCell(x,y) == "black")
-            iv.setBackgroundColor(ContextCompat.getColor(this,resources.getIdentifier(nameColorBlack,"color", packageName)))
-        //white cell
-        else
-            iv.setBackgroundColor(ContextCompat.getColor(this,resources.getIdentifier(nameColorWhite,"color", packageName)))
-        //horse
-        if(board[x][y] == 1)
-            iv.setBackgroundColor(ContextCompat.getColor(this,resources.getIdentifier("previous_cell","color", packageName)))
-
+    private fun setFirstPosition(){
+        var x = 0
+        var y = 0
+        x = (0..7).random()
+        y = (0..7).random()
+        cellSelected_x = x
+        cellSelected_y = y
+        selectCell(x,y)
     }
 
     private fun checkNewBonus(){
@@ -270,19 +187,75 @@ class MainActivity : AppCompatActivity() {
             paintBonusCell(bonusCell_x, bonusCell_y)
         }
     }
-
     private fun paintBonusCell(x:Int, y:Int){
         var iv: ImageView = findViewById(resources.getIdentifier("c$x$y","id",packageName))
         iv.setImageResource(R.drawable.ic_bonus)
     }
+    private fun growProgressBonus(){
+        var moves_done = levelMoves - moves
+        var bonus_done = moves_done / movesRequired
+        var moves_reset = movesRequired * bonus_done
+        var bonus_grow = moves_done - moves_reset
 
-    private fun checkGameOver(x:Int, y:Int){
-        if (options == 0){
-            if (bonus == 0)
-                showMessage ("Game Over", "Try again", true)
+        var v = findViewById<View>(R.id.vNewBonus)
+        var widthBonus = ((width_bonus/movesRequired) * bonus_grow).toFloat()
+
+        var height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8f, resources.displayMetrics).toInt()
+        var width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, widthBonus, resources.displayMetrics).toInt()
+        v.setLayoutParams(TableRow.LayoutParams(width,height))
+    }
+
+    private fun clearOption(x:Int, y:Int){
+        var iv: ImageView = findViewById(resources.getIdentifier("c$x$y","id",packageName))
+        //black cell
+        if (checkColorCell(x,y) == "black")
+            iv.setBackgroundColor(ContextCompat.getColor(this,resources.getIdentifier(nameColorBlack,"color", packageName)))
+        //white cell
+        else
+            iv.setBackgroundColor(ContextCompat.getColor(this,resources.getIdentifier(nameColorWhite,"color", packageName)))
+        //horse
+        if(board[x][y] == 1)
+            iv.setBackgroundColor(ContextCompat.getColor(this,resources.getIdentifier("previous_cell","color", packageName)))
+
+    }
+    private fun clearOptions(){
+        for(i in 0..7){
+            for(j in 0..7){
+                if(board[i][j] == 9 || board[i][j] == 2){
+                    if(board[i][j] == 9)
+                        board[i][j] = 0
+                    clearOption(i,j)
+                }
+            }
+        }
+    }
+    private fun paintOption(x:Int, y:Int){
+        var iv: ImageView = findViewById(resources.getIdentifier("c$x$y","id",packageName))
+        if(checkColorCell(x,y) == "black") iv.setBackgroundResource(R.drawable.option_black)
+        else iv.setBackgroundResource(R.drawable.option_white)
+    }
+    private fun paintAllOptions(){
+        for(i in 0..7){
+            for(j in 0..7){
+                if(board[i][j] != 1)
+                    paintOption(i,j)
+                if(board[i][j] == 0)
+                    board[i][j] = 9
+            }
         }
     }
 
+    private fun checkGameOver(){
+        if (options == 0){
+            if (bonus > 0){
+                checkMovement = false
+                paintAllOptions()
+            }
+            else
+                showMessage ("Game Over", "Try again", true)
+
+        }
+    }
     private fun showMessage(title:String, action:String, gameOver:Boolean){
         var lyMessage = findViewById<LinearLayout>(R.id.lyMessage)
         lyMessage.visibility = View.VISIBLE
@@ -301,5 +274,48 @@ class MainActivity : AppCompatActivity() {
         var tvAction = findViewById<TextView>(R.id.tvAction)
         tvAction.text = action
 
+    }
+
+    private fun checkOption(x:Int, y:Int){
+        options = 0
+        checkMove(x,y,1,2)      //1 right, 2 up
+        checkMove(x,y,2,1)      //2 right, 1 up
+        checkMove(x,y,1,-2)     //1 right, 2 bottom
+        checkMove(x,y,2,-1)     //2 right, 1 bottom
+        checkMove(x,y,-1,2)     //1 left, 2 up
+        checkMove(x,y,-2,1)     //2 left, 1 up
+        checkMove(x,y,-1,-2)    //1 left, 2 bottom
+        checkMove(x,y,-2,-1)    //2 left, 1 bottom
+
+        var tvOptionsData = findViewById<TextView>(R.id.tvOptionsData)
+        tvOptionsData.text = options.toString()
+    }
+    private fun checkMove(x:Int, y:Int, move_x:Int, move_y:Int){
+        var option_x = x + move_x
+        var option_y = y + move_y
+        //check if the move is not out of the board
+        if (option_x < 8 && option_y < 8 && option_x >= 0 && option_y >= 0)
+            if(board[option_x][option_y] == 0 || board[option_x][option_y] == 2){
+                options++
+                paintOption(option_x,option_y)
+                if (board[option_x][option_y] == 0)
+                    board[option_x][option_y] = 9
+            }
+
+    }
+    private fun checkColorCell(x:Int, y:Int):String{
+        var color = "white"
+        var blackColumn_x = arrayOf(0,2,4,6)
+        var blackRow_x = arrayOf(1,3,5,7)
+        if ((blackColumn_x.contains(x) && blackColumn_x.contains(y))
+            || (blackRow_x.contains(x) && blackRow_x.contains(y)))
+            color = "black"
+        return color
+    }
+
+    private fun paintHorseCell(x:Int, y:Int, color:String){
+        var iv: ImageView = findViewById(resources.getIdentifier("c$x$y","id",packageName))
+        iv.setBackgroundColor(ContextCompat.getColor(this,resources.getIdentifier(color,"color", packageName)))
+        iv.setImageResource(R.drawable.ic_horse)
     }
 }
